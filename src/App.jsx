68d54ba6899e12f99fcd2ffa3d22538cfc43f340
your_name_here <rautsagar214@gmail.com>
2024-01-06 15:27:42 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -23,8 +23,40 @@ const imageData = ()=>{
   return data;
 }
 
+let data = imageData()
+
+
 function App() {
-  // code here
+  
+  let galleryStyle = {
+    textAlign:"center"
+  }
+  let styleMyDiv = {
+    display:"grid",
+    gridTemplateColumns:"48.5% 48.5%",
+    gridGap:"1%"
+  }
+  return(
+  <>
+    <h1 style={galleryStyle}>Kalvium Gallery</h1>
+
+    {/* <div style={styleMyDiv}>
+      <img src={data[0].img} alt="" width={"200px"} />
+      <img src={data[1].img} alt="" width={"200px"} />
+      <img src={data[2].img} alt="" width={"200px"} />
+      <img src={data[3].img} alt="" width={"200px"} />
+      
+    </div> */}
+
+    <div style={styleMyDiv}>
+      {data.map((el)=>(
+        <div key={el.id}>
+       <img src={el.img} width={"100%"} />
+      </div>
+      ))}
+   </div>
+    </>
+  )
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -27,4 +27,32 @@ export default class AppClass extends Component{
   }
 
   // code here
+  render(){
+    this.data = this.imageData()
+    console.log(this.data)
+    
+    let galleryStyle = {
+      textAlign:"center"
+    }
+    let styleMyDiv = {
+      display:"grid",
+      gridTemplateColumns:"48.5% 48.5%",
+      gridGap:"1%"
+    }
+    return(
+      <>
+        <h1 style={galleryStyle}>Kalvium Gallery</h1>
+    
+        <div style={styleMyDiv}>
+          <img src={this.data[0].img} alt="" width={"100%"} />
+          <img src={this.data[1].img} alt="" width={"100%"} />
+          <img src={this.data[2].img} alt="" width={"100%"} />
+          <img src={this.data[3].img} alt="" width={"100%"} />
+          
+        </div>
+    
+        
+        </>
+      )
+  }
 }
